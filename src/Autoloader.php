c74aced459b8b0bf1fcdd7670b9379546f17d039
Guillaume de Matos guillaume.de-matos@epitech.eu <guillaume.de-matos@epitech.eu>
2019-01-26 16:38:32 +0100

    TMP: Attempt to add autoloading for Puncto app

--- src/Autoloader.php
+++ src/Autoloader.php
@@ -7,6 +7,8 @@ use \RecursiveIteratorIterator;
 
 abstract class Autoloader
 {
+    const APP_NAMESPACE = "PunctoInternalApp";
+
     private static function cleanPath($path, $trimStart = false)
     {
         $result = rtrim($path, DIRECTORY_SEPARATOR);
@@ -27,30 +29,28 @@ abstract class Autoloader
         define('__ROOT__', self::cleanPath($base, false));
         define('__APP__', self::cleanPath($app));
 
-        // spl_autoload_register(function ($fqcn) {
-        //     $segments = explode('\\', $fqcn);
+        spl_autoload_register(function ($fqcn) {
+            $segments = explode('\\', $fqcn);
 
-        //     $namespace = $segments[0];
-        //     $klass = array_values(array_slice($segments, -1))[0];
+            $namespace = $segments[0];
+            $klass = array_values(array_slice($segments, -1))[0];
 
-        //     if ($namespace === 'Puncto') {
-        //         require __DIR__ . DIRECTORY_SEPARATOR . "classes" . DIRECTORY_SEPARATOR . "$klass.php";
-        //     } else {
-        //         $appRoot = __ROOT__ . DIRECTORY_SEPARATOR . __APP__;
-        //         $filename = "$klass.php";
+            if ($namespace === self::APP_NAMESPACE) {
+                $appRoot = __ROOT__ . DIRECTORY_SEPARATOR . __APP__;
+                $filename = "$klass.php";
 
-        //         $directory = new RecursiveDirectoryIterator($appRoot, RecursiveDirectoryIterator::SKIP_DOTS);
-        //         $iterator = new RecursiveIteratorIterator($directory, RecursiveIteratorIterator::LEAVES_ONLY);
+                $directory = new RecursiveDirectoryIterator($appRoot, RecursiveDirectoryIterator::SKIP_DOTS);
+                $iterator = new RecursiveIteratorIterator($directory, RecursiveIteratorIterator::LEAVES_ONLY);
 
-        //         foreach ($iterator as $file) {
-        //             if (strtolower($file->getFilename()) === strtolower($filename)) {
-        //                 if ($file->isReadable()) {
-        //                     require $file->getPathname();
-        //                 }
-        //                 break;
-        //             }
-        //         }
-        //     }
-        // });
+                foreach ($iterator as $file) {
+                    if (strtolower($file->getFilename()) === strtolower($filename)) {
+                        if ($file->isReadable()) {
+                            require $file->getPathname();
+                        }
+                        break;
+                    }
+                }
+            }
+        });
     }
 }
