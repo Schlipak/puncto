92c775ffe3da67a09e51da70aa3afb475402c4ed
Guillaume de Matos guillaume.de-matos@epitech.eu <guillaume.de-matos@epitech.eu>
2019-01-27 19:32:37 +0100

    Added template circular dependency detection

--- src/Renderer.php
+++ src/Renderer.php
@@ -33,15 +33,78 @@ namespace Puncto {
             return array_key_exists($name, $this->context);
         }
 
+        private function findCircularDeps($template, $ext = 'html.php')
+        {
+            $backtrace = debug_backtrace();
+            $results = ['circular' => false, 'stack' => []];
+
+            foreach ($backtrace as $trace) {
+                if (isset($trace['file'])) {
+                    $file = $trace['file'];
+
+                    if ($file === $template) {
+                        $results['circular'] = true;
+                    }
+
+                    if (preg_match("/$ext$/", $file)) {
+                        $results['stack'][] = $file;
+                    }
+                }
+            }
+
+            return $results;
+        }
+
+        private function renderCircularDepError($results, $currentTemplate)
+        {
+            if ($results['circular']) {
+                $stack = array_merge([$currentTemplate], $results['stack']);
+                $list = implode('', array_map(function ($file) {
+                    return "<li>$file</li>";
+                }, $stack));
+                $message = "Circular dependency detected<ol start='0'>$list</ol>";
+
+                throw new \Error($message);
+            }
+        }
+
+        public function expandPath($filename)
+        {
+            return (
+                __ROOT__ .
+                DIRECTORY_SEPARATOR .
+                __APP__ .
+                DIRECTORY_SEPARATOR .
+                'templates' .
+                DIRECTORY_SEPARATOR .
+                $filename
+            );
+        }
+
+        public function partial($name, $ext = 'html.php')
+        {
+            $backtrace = debug_backtrace();
+            $caller = $backtrace[0]['file'];
+            $base = dirname($caller);
+            $filename = "_$name.$ext";
+            $fullName = $this->expandPath("partials/$filename");
+
+            error_log("    Include partial $filename");
+
+            $this->renderCircularDepError($this->findCircularDeps($fullName), $fullName);
+            return $fullName;
+        }
+
         public function render($template, $expandPath = true, $ext = 'html.php')
         {
             $__templateFile = "$template.$ext";
-
             error_log("  Rendering template $__templateFile");
 
             if ($expandPath) {
-                $__templateFile = __ROOT__ . DIRECTORY_SEPARATOR . __APP__ . "/templates/$__templateFile";
+                $__templateFile = $this->expandPath($__templateFile);
             }
+
+            $this->renderCircularDepError($this->findCircularDeps($__templateFile), $__templateFile);
 
             extract($this->context);
             RendererDefineHelpers();
